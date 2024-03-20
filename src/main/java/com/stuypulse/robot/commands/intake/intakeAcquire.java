d1fbe3fb5a5b3737a0e059063d0136fd5fa8e5fd
hahafoot <gordondoan13@gmail.com>
2024-03-20 17:35:19 -0400

    added intake code, commands.
    Co-authored-by: naveed1117 <naveed1117@users.noreply.github.com>
    Co-authored-by: jopy-man <jopy-man@users.noreply.github.com>
    Co-authored-by: lapcas <lapcas@users.noreply.github.com>

---
+++ src/main/java/com/stuypulse/robot/commands/intake/intakeAcquire.java
@@ -0,0 +1,7 @@
+package com.stuypulse.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class intakeAcquire extends Command{
+    
+}
---
+++ src/main/java/com/stuypulse/robot/subsystems/Intake.java
@@ -0,0 +1,39 @@
+package com.stuypulse.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase{
+    private final int MOTOR_PORT = 0;
+    private final int ACQUIRE_SPEED = 0;
+
+    private final CANSparkMax motor;
+
+    public Intake() {
+        motor = new CANSparkMax(MOTOR_PORT, MotorType.kBrushless);
+    }
+
+    public void acquire() {
+        motor.set(ACQUIRE_SPEED * 1);
+    }
+
+    public void deacquire() {
+        motor.set(ACQUIRE_SPEED * -1);
+    }
+
+    public void stop() {
+        motor.stopMotor();
+    }
+    
+    @Override
+    public void periodic() {
+        super.periodic();
+
+        SmartDashboard.putNumber("Intake/Speed", motor.get());
+        SmartDashboard.putNumber("Intake/Current", motor.getOutputCurrent());
+    }
+}
