f4d88e387a7feb53ff6f825375b434ca4fe3f055
zixifeng12 <zixifeng666888@gamil.com>
2024-03-26 18:36:19 -0400

    removed used configs

--- src/main/java/com/stuypulse/robot/constants/Motors.java
+++ src/main/java/com/stuypulse/robot/constants/Motors.java
@@ -149,47 +149,5 @@ public interface Motors {
             motor.follow(follows);
             motor.burnFlash();
          }
-     }
-     public interface TankDrive {
-        int CURRENT_LIMIT_AMPS = 60;
-        IdleMode kBrake = null;
-        IdleMode IDLE_MODE = kBrake;
-
-        Config LEFT = new Config(true, IDLE_MODE, CURRENT_LIMIT_AMPS);
-        Config RIGHT = new Config(false, IDLE_MODE, CURRENT_LIMIT_AMPS);
-    }
-
-    public static class Config {
-        public final boolean INVERTED;
-        public final IdleMode IDLE_MODE;
-        public final int CURRENT_LIMIT_AMPS;
-        public final double OPEN_LOOP_RAMP_RATE;
-
-        public Config(
-                boolean inverted,
-                IdleMode idleMode,
-                int currentLimitAmps,
-                double openLoopRampRate) {
-            this.INVERTED = inverted;
-            this.IDLE_MODE = idleMode;
-            this.CURRENT_LIMIT_AMPS = currentLimitAmps;
-            this.OPEN_LOOP_RAMP_RATE = openLoopRampRate;
-        }
-
-        public Config(boolean inverted, IdleMode idleMode, int currentLimitAmps) {
-            this(inverted, idleMode, currentLimitAmps, 0.0);
-        }
-
-        public Config(boolean inverted, IdleMode idleMode) {
-            this(inverted, idleMode, 80);
-        }
-
-        public void configure(CANSparkMax motor) {
-            motor.setInverted(INVERTED);
-            motor.setIdleMode(IDLE_MODE);
-            motor.setSmartCurrentLimit(CURRENT_LIMIT_AMPS);
-            motor.setOpenLoopRampRate(OPEN_LOOP_RAMP_RATE);
-            motor.burnFlash();
-        }
-    }
+    }    
 }
