90df8b776ac06c9203696c935568df1df84c39ef
hahafoot <gordondoan13@gmail.com>
2024-03-26 16:50:05 -0400

    Wrote usage docs for Shooter :3
    Co-authored-by: Lapcas <Lapcas@users.noreply.github.com>
    Co-authored-by: jopy-man <jopy-man@users.noreply.github.com>

---
+++ src/main/java/com/stuypulse/robot/subsystems/ShooterSparkFlex.java
@@ -0,0 +1,83 @@
+package com.stuypulse.robot.subsystems;
+
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSparkFlex extends SubsystemBase{
+    public int leftMotorPort = 0;
+    public int rightMotorPort = 1;
+
+    private double targetLeftRPM;
+    private double targetRightRPM; 
+ 
+    private CANSparkFlex leftMotor;
+    private CANSparkFlex rightMotor;
+
+    private RelativeEncoder leftEncoder;
+    private RelativeEncoder rightEncoder;
+
+    private SimpleMotorFeedforward leftController;
+    private SimpleMotorFeedforward rightController;
+
+    public ShooterSparkFlex() {
+        leftMotor = new CANSparkFlex(leftMotorPort, MotorType.kBrushless);
+        rightMotor = new CANSparkFlex(rightMotorPort, MotorType.kBrushless);
+
+        leftEncoder = leftMotor.getEncoder();
+        rightEncoder = rightMotor.getEncoder();
+
+        leftController = new SimpleMotorFeedforward(0, 0);
+        rightController = new SimpleMotorFeedforward(0, 0);
+    }
+
+    public void shoot(double leftRPM, double rightRPM) {
+        targetLeftRPM = leftRPM;
+        targetRightRPM = rightRPM;
+    }
+
+    public void shootStop() {
+        leftMotor.stopMotor();
+        rightMotor.stopMotor();
+    }
+
+    public double getLeftVelocity() {
+        return leftEncoder.getVelocity();
+    }
+
+    public double getRightVelocity() {
+        return rightEncoder.getVelocity();
+    }
+
+    public double getTargetLeftRPM() {
+        return targetLeftRPM;
+    }
+
+    public double getTargetRightRPM() {
+        return targetRightRPM;
+    }
+
+    public double getLeftVoltage() {
+        return leftMotor.getBusVoltage();
+    }
+
+    public double getRightVoltage() {
+        return rightMotor.getBusVoltage();
+    }
+
+    @Override
+    public void periodic() {
+        leftMotor.setVoltage(leftController.calculate(targetLeftRPM));
+        rightMotor.setVoltage(rightController.calculate(targetRightRPM));
+
+        SmartDashboard.putNumber("Shooter/Left Velocity", getLeftVelocity());
+        SmartDashboard.putNumber("Shooter/Right Velocity", getRightVelocity());
+        SmartDashboard.putNumber("Shooter/Left Target RPM",getTargetLeftRPM());
+        SmartDashboard.putNumber("Shooter/Right Target RPM", getTargetRightRPM());
+        SmartDashboard.putNumber("Shooter/Left Voltage", getLeftVoltage());
+        SmartDashboard.putNumber("Shooter/Right Voltage", getRightVoltage());
+    }
+}
